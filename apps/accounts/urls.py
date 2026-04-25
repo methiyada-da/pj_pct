c8702d5ba7ccf6bb8cd0be5e8185ca9d50cad08a
MatchaWannoi <rocklovely70@gmail.com>
2026-04-25 08:07:19 +0700

    mail

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('password-reset/done/',                             views.password_reset_done_view,     name='password_reset_done'),
     path('password-reset/confirm/<uidb64>/<token>/',         views.password_reset_confirm_view,  name='password_reset_confirm'),
     path('password-reset/complete/',                         views.password_reset_complete_view, name='password_reset_complete'),
-    path('profile/',        views.profile_view,        name='profile'),
+    path('profile/',          views.profile_view,          name='profile'),
+    path('register/pending/',    views.register_pending_view, name='register_pending'),
+    path('verify-email/',        views.verify_email_view,     name='verify_email'),
+    path('verify-email/cancel/', views.cancel_verify_view,    name='verify_email_cancel'),
 ]
