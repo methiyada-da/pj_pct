45ad9cb1c5792208710c83ca05c8c449e7a3445e
methiyada <mewmethiyada11@gmail.com>
2026-08-03 10:06:19 +0700

    Co-authored-by: Matcha <MatchaWannoi@users.noreply.github.com> Co-authored-by: nattanatta1234 <nattanatta1234@users.noreply.github.com>

--- apps/bookings/apps.py
+++ apps/bookings/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class BookingsConfig(AppConfig):
     name = 'apps.bookings'
+
+    def ready(self):
+        import apps.bookings.signals  # โหลดสัญญาณเมื่อแอปพร้อม
